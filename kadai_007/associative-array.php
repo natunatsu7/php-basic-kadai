bc460cd5e76c3bce9dd32a1523bfac246e601ecb
natunatsu7 <118952809+natunatsu7@users.noreply.github.com>
2023-11-18 21:34:48 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>PHP課題007</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    //連数配列の作成
+    $series_array= ['name' => 'orion', 'price' => 200, 'weight' => '160'];
+
+    //連数配列を出力する
+    print_r($series_array);
