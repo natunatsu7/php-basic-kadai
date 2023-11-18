be65223b40badb8fb9161870b972bfa368c8ff85
natunatsu7 <118952809+natunatsu7@users.noreply.github.com>
2023-11-18 23:39:34 +0900

    Add files via upload

---
+++ kadai_011/foreach.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>PHP課題11</title>
+</head>
+
+<body>
+  <p>
+    <?php
+
+    //連想配列の作成
+    $food = ['名前' => '玉ねぎ', '値段' => 200, '産地' => '北海道']; 
+
+    foreach ($food as $key => $value) {
+      echo "{$key}：{$value}<br>";
+    }
+    ?>
+  </p>
+</body>
+
+<html>
